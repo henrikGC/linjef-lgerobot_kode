e4079f25f9510d6b0ee5afc0097f907eb3d17dcc
Henrik Ceballos <henrikgceballos@gmail.com>
2026-09-22 14:51:58 +0200

    Sikkerhetscommit før chat ødelegger

--- sensor_farge_avlesning.ino
+++ sensor_farge_avlesning.ino
@@ -5,110 +5,123 @@ QTRSensors qtr;
 const uint8_t SensorCount = 6;
 uint16_t sensorValues[SensorCount];
 
-// PIN VARIABLES
-// the motor will be controlled by the motor A pins on the motor driver
-const int AIN1 = 13; // control pin 1 on the motor driver for the right motor
-const int AIN2 = 12; // control pin 2 on the motor driver for the right motor
-const int PWMA = 11; // speed control pin on the motor driver for the right motor
+const int AIN1 = 13;
+const int AIN2 = 12;
+const int PWMA = 11;
 const int BIN1 = 8;
 const int BIN2 = 9;
 const int PWMB = 10;
 
-// VARIABLES
-int motorSpeed = 0; // starting speed for the motor
+const int speed = 175;
+int motorSpeed = 0;
+
+
+bool isCalibrated = false;
+bool isMotorOn = true;
+
+bool isPrintingSensorData = true;
+bool isPrintingPosition = true;
+
+
+class Motor {
+
+  private:
+    int IN1;
+    int IN2;
+    int PWM; 
+
+
+  public:
+
+    Motor(int in1, int in2, int pwm){
+      IN1 = in1;
+      IN2 = in2;
+      PWM = pwm;
+    }
+
+    void begin() {
+      pinMode(IN1, OUTPUT);
+      pinMode(IN2, OUTPUT);
+      pinMode(PWM, OUTPUT);
+    }
+
+    void driveForward(int speed) {
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, HIGH);
+        analogWrite(PWM, speed);
+    }
+
+    void driveBackward(int speed) {
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      analogWrite(PWM, speed);
+    }
+    void stop() {
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      analogWrite(PWM, 0);
+    }
+
+};
+
+Motor rightMotor(AIN1, AIN2, PWMA);
+Motor leftMotor(BIN1, BIN2, PWMB);
 
 void setup()
 {
-  // configure the sensors
-  qtr.setTypeAnalog();
-  qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4, A5}, SensorCount);
+  Serial.begin(9600);
+
+  leftMotor.begin();
+      rightMotor.begin();
+
+  // Zumo reflectance array bruker RC
+  qtr.setTypeRC();
+
+  // Sensorene i fysisk rekkefølge
+  qtr.setSensorPins(
+    (const uint8_t[]){4, A3, 6, A0, A2, 5},
+    SensorCount
+  );
+
+  // LEDON koblet til D2
   qtr.setEmitterPin(2);
 
-  pinMode(AIN1, OUTPUT);
-  pinMode(AIN2, OUTPUT);
-  pinMode(PWMA, OUTPUT);
-  pinMode(BIN1, OUTPUT);
-  pinMode(BIN2, OUTPUT);
-  pinMode(PWMB, OUTPUT);
-
-  delay(500);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH); // turn on Arduino's LED to indicate we are in calibration mode
-
-  // analogRead() takes about 0.1 ms on an AVR.
-  // 0.1 ms per sensor * 4 samples per sensor read (default) * 6 sensors
-  // * 10 reads per calibrate() call = ~24 ms per calibrate() call.
-  // Call calibrate() 400 times to make calibration take about 10 seconds.
-  for (uint16_t i = 0; i < 400; i++)
-  {
-    qtr.calibrate();
-  }
-  digitalWrite(LED_BUILTIN, LOW); // turn off Arduino's LED to indicate we are through with calibration
-
-  // print the calibration minimum values measured when emitters were on
-  Serial.begin(9600);
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(qtr.calibrationOn.minimum[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-
-  // print the calibration maximum values measured when emitters were on
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(qtr.calibrationOn.maximum[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-  Serial.println();
-  delay(1000);
+  Serial.println("Sensor test starter...");
 }
 
 void loop()
 {
-  // read calibrated sensor values and obtain a measure of the line position
-  // from 0 to 5000 (for a white line, use readLineWhite() instead)
-  uint16_t position = qtr.readLineBlack(sensorValues);
+  // Leser råverdiene fra alle 6 sensorene
+  qtr.read(sensorValues);
 
-  // print the sensor values as numbers from 0 to 1000, where 0 means maximum
-  // reflectance and 1000 means minimum reflectance, followed by the line
-  // position
   for (uint8_t i = 0; i < SensorCount; i++)
   {
     Serial.print(sensorValues[i]);
     Serial.print('\t');
   }
+
+  Serial.println();
+  uint16_t position = qtr.readLineBlack(sensorValues);
   Serial.println(position);
-  int middle_sensor = sensorValues[2]+sensorValues[3];
-  int left_sensor = sensorValues[0]+sensorValues[1];
-  int right_sensor = sensorValues[4]+sensorValues[5];
+  // position = 1000;
+  if (position > 2000 && position < 3000){
+        Serial.println("forward");
+        rightMotor.driveForward(speed);
+        leftMotor.driveForward(speed);
+      } else if (position <= 2000)
+      {
+        Serial.println("left");
+        leftMotor.driveForward(speed);
+        rightMotor.driveBackward(speed);
+        //turn left;
+      } else if (position >= 3000) {
+        Serial.println("right");
+        leftMotor.driveBackward(speed);
+        rightMotor.driveForward(speed);
+        //turn right;
+      } else {
+        Serial.println("ERROR");
+      }
 
-  if (middle_sensor < left_sensor && right_sensor < left_sensor) {  
-    digitalWrite(AIN1, LOW);  // set pin 1 to low
-    digitalWrite(AIN2, HIGH); // set pin 2 to high
-    analogWrite(PWMA, 100);
-    digitalWrite(BIN1, HIGH);  // set pin 1 to low
-    digitalWrite(BIN2, LOW); // set pin 2 to high
-    analogWrite(PWMB, 100);
-  } 
-  else if (middle_sensor < right_sensor && right_sensor > left_sensor) {
-    digitalWrite(AIN1, LOW); // set pin 1 to low
-    digitalWrite(AIN2, LOW); // set pin 2 to low
-    analogWrite(PWMA, 0);
-    digitalWrite(BIN1, LOW); // set pin 1 to low
-    digitalWrite(BIN2, LOW); // set pin 2 to low
-    analogWrite(PWMB, 0);
-  }
-  else {
-    digitalWrite(AIN1, HIGH); // set pin 1 to high
-    analogWrite(PWMA, 100);
-    digitalWrite(BIN1, LOW); // set pin 1 to high
-    digitalWrite(BIN2, HIGH);  // set pin 2 to low
-    digitalWrite(AIN2, LOW);  // set pin 2 to low
-    analogWrite(PWMB, 100);    
-  }
-  
+  delay(250);
 }
-
-//https://learn.sparkfun.com/tutorials/activity-guide-for-sparkfun-tinker-kit/circuit-10-motor-basics
