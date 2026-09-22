6e0bdb5232ff08f7e2053d6981487142d11f833f
Henrik Ceballos <henrikgceballos@gmail.com>
2026-09-22 17:09:12 +0200

    legg til knapp

--- sensor_farge_avlesning.ino
+++ sensor_farge_avlesning.ino
@@ -5,6 +5,9 @@ QTRSensors qtr;
 const uint8_t SensorCount = 6;
 uint16_t sensorValues[SensorCount];
 
+const int switchPin = 3;
+bool isMotorOn = false;
+
 const int AIN1 = 13;
 const int AIN2 = 12;
 const int PWMA = 11;
@@ -17,7 +20,6 @@ int motorSpeed = 0;
 
 
 bool isCalibrated = false;
-bool isMotorOn = true;
 
 bool isPrintingSensorData = true;
 bool isPrintingPosition = true;
@@ -28,12 +30,12 @@ class Motor {
   private:
     int IN1;
     int IN2;
-    int PWM; 
+    int PWM;
 
 
   public:
 
-    Motor(int in1, int in2, int pwm){
+    Motor(int in1, int in2, int pwm) {
       IN1 = in1;
       IN2 = in2;
       PWM = pwm;
@@ -46,9 +48,9 @@ class Motor {
     }
 
     void driveForward(int speed) {
-        digitalWrite(IN1, LOW);
-        digitalWrite(IN2, HIGH);
-        analogWrite(PWM, speed);
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      analogWrite(PWM, speed);
     }
 
     void driveBackward(int speed) {
@@ -56,6 +58,7 @@ class Motor {
       digitalWrite(IN2, LOW);
       analogWrite(PWM, speed);
     }
+
     void stop() {
       digitalWrite(IN1, LOW);
       digitalWrite(IN2, LOW);
@@ -63,6 +66,7 @@ class Motor {
     }
 
 };
+
 
 Motor rightMotor(AIN1, AIN2, PWMA);
 Motor leftMotor(BIN1, BIN2, PWMB);
@@ -72,7 +76,7 @@ void setup()
   Serial.begin(9600);
 
   leftMotor.begin();
-      rightMotor.begin();
+  rightMotor.begin();
 
   // Zumo reflectance array bruker RC
   qtr.setTypeRC();
@@ -86,13 +90,24 @@ void setup()
   // LEDON koblet til D2
   qtr.setEmitterPin(2);
 
+  for (uint16_t i = 0; i < 400; i++) {
+    qtr.calibrate();
+    delay(10);
+  }
+
   Serial.println("Sensor test starter...");
 }
 
 void loop()
 {
+  bool switching = digitalRead(switchPin);
+  if (switching) {
+    isMotorOn = !isMotorOn;
+  }
+  
   // Leser råverdiene fra alle 6 sensorene
-  qtr.read(sensorValues);
+  //qtr.read(sensorValues);
+  qtr.readCalibrated(sensorValues);
 
   for (uint8_t i = 0; i < SensorCount; i++)
   {
@@ -101,27 +116,55 @@ void loop()
   }
 
   Serial.println();
+  
   uint16_t position = qtr.readLineBlack(sensorValues);
   Serial.println(position);
-  // position = 1000;
-  if (position > 2000 && position < 3000){
-        Serial.println("forward");
-        rightMotor.driveForward(speed);
-        leftMotor.driveForward(speed);
-      } else if (position <= 2000)
-      {
-        Serial.println("left");
-        leftMotor.driveForward(speed);
-        rightMotor.driveBackward(speed);
-        //turn left;
-      } else if (position >= 3000) {
-        Serial.println("right");
-        leftMotor.driveBackward(speed);
-        rightMotor.driveForward(speed);
-        //turn right;
-      } else {
-        Serial.println("ERROR");
-      }
 
+    // position = 1000;
+
+  if (isMotorOn) {
+
+  if (position > 2000 && position < 3000)
+  {
+    Serial.println("forward");
+
+    rightMotor.driveForward(speed);
+    leftMotor.driveForward(speed);
+  }
+
+  else if (position <= 2000)
+  {
+    Serial.println("left");
+
+    leftMotor.driveForward(speed);
+    rightMotor.driveBackward(speed);
+
+    //turn left;
+  }
+
+  else if (position >= 3000)
+  {
+    Serial.println("right");
+
+    leftMotor.driveBackward(speed);
+    rightMotor.driveForward(speed);
+
+    //turn right;
+  }
+
+  else
+  {
+    Serial.println("ERROR");
+  }
+  }
+  else 
+{
+  // Bryteren er AV
+  rightMotor.stop();
+  leftMotor.stop();
+
+  Serial.println("MOTORS OFF");
+}
+  
   delay(250);
 }
